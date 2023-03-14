57981b45ccc87ae51e2b12deb21d45df5d9c1766
Prensor7 <127869750+Prensor7@users.noreply.github.com>
2023-03-14 10:52:29 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Servidor {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
 
         //Middlewares
